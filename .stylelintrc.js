fa072f9bcf9d69267697d67f4b24e2433c62dab2
Ned Zimmerman <ned@bight.ca>
2019-08-13 11:03:50 -0300

    Switch to CSS, match platform-coop-toolkit/pcc configuration

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,13 +2,23 @@ module.exports = {
   'extends': 'stylelint-config-standard',
   'rules': {
     'no-empty-source': null,
+    'no-descending-specificity': null,
     'string-quotes': 'double',
+    'media-feature-name-no-unknown': [
+      true,
+      {
+        'ignoreMediaFeatureNames': [
+          'prefers-reduced-motion'
+        ]
+      }
+    ],
     'at-rule-no-unknown': [
       true,
       {
         'ignoreAtRules': [
           'extend',
           'at-root',
+          'custom-media',
           'debug',
           'warn',
           'error',
--- resources/assets/build/postcss.config.js
+++ resources/assets/build/postcss.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-
 const cssnanoConfig = {
   preset: ['default', { discardComments: { removeAll: true } }]
 };
@@ -8,6 +7,14 @@ module.exports = ({ file, options }) => {
   return {
     parser: options.enabled.optimize ? 'postcss-safe-parser' : undefined,
     plugins: {
+      'postcss-import': {},
+      'postcss-partial-import': {},
+      'postcss-custom-media': {},
+      'postcss-custom-properties': {
+        importFrom: './resources/assets/styles/common/_properties.css',
+        preserve: false,
+      },
+      'postcss-calc': {},
       autoprefixer: true,
       cssnano: options.enabled.optimize ? cssnanoConfig : false,
     },
