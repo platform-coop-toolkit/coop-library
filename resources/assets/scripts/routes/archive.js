af743de5a224ba617dac274ea6c4ed2f25811fdd
Ned Zimmerman <ned@bight.ca>
2019-12-03 11:31:34 -0700

    Fix bad function name

--- resources/assets/scripts/routes/archive.js
+++ resources/assets/scripts/routes/archive.js
@@ -1,7 +1,7 @@
 export default {
   init() {
     // JavaScript to be fired on the home page
-    window.pinecone.card();
+    window.pinecone.cards();
     window.pinecone.accordions();
   },
   finalize() {
