daa26279f26f889205bf3c30985753544b528cb2
Ned Zimmerman <ned@bight.ca>
2019-12-04 16:32:40 -0700

    Remove no-js

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  {{-- <script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script> --}}
+  <script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>
   @php wp_head() @endphp
 </head>
