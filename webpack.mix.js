cda053c24affb8f34abb3cedcf901a286179879d
Ned Zimmerman <ned@bight.ca>
2019-12-04 09:16:33 -0700

    Fix asset manifest (#82)

--- webpack.mix.js
+++ webpack.mix.js
@@ -37,3 +37,14 @@ mix.options({
 
 mix.sourceMaps(false, 'source-map')
    .version();
+
+mix.then(() => {
+  const manifest = File.find('./dist/mix-manifest.json');
+  const json = JSON.parse(manifest.read());
+  Object.keys(json).forEach(key => {
+    const hashed = json[key];
+    delete json[key];
+    json[key.replace(/^\/+/g, '')] = hashed.replace(/^\/+/g, '');
+  });
+  manifest.write(JSON.stringify(json, null, 2));
+});
