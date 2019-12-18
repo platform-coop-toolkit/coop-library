ac4c9622473e1208eb84fc2dacf9e460123d3b5e
Ned Zimmerman <ned@bight.ca>
2019-12-18 16:53:24 -0700

    Import new scripts

--- resources/assets/scripts/routes/archive.js
+++ resources/assets/scripts/routes/archive.js
@@ -1,11 +1,38 @@
-import { Accordion } from '@platform-coop-toolkit/pinecone';
+import Pinecone from '@platform-coop-toolkit/pinecone';
 
 export default {
   init() {
     // JavaScript to be fired on the Archive page
+    const cards = document.querySelectorAll( '.card' );
+
+    if ( cards ) {
+      Array.prototype.forEach.call( cards, card => {
+        new Pinecone.Card( card );
+      } );
+    }
+
+    const filterContainer = document.querySelector( '.filter-sort__filters' );
+    const showFilters = document.querySelector( '#show-filters' );
+    const hideFilters = document.querySelector( '#hide-filters' );
+
+    if ( showFilters && hideFilters && filterContainer ) {
+      new Pinecone.FilterList( filterContainer, showFilters, hideFilters );
+    }
+
+    const nestedCheckboxContainers = document.querySelectorAll( '.input-group__parent > li' );
+    if ( nestedCheckboxContainers ) {
+      Array.prototype.forEach.call( nestedCheckboxContainers, container => {
+        const input = container.querySelector( 'li > input' );
+        const subInputs = container.querySelectorAll( '.input-group__descendant input' );
+        if ( 0 < subInputs.length ) {
+          new Pinecone.NestedCheckbox( container, input, subInputs );
+        }
+      } );
+    }
+
     const accordions = document.querySelectorAll('.accordions');
     Array.prototype.forEach.call( accordions, accordion => {
-      new Accordion(
+      new Pinecone.Accordion(
         accordion,
         {
           paneSelector: '.accordion',
--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,20 @@
+import Pinecone from '@platform-coop-toolkit/pinecone';
+
 export default {
   init() {
     // JavaScript to be fired on all pages
+    const toggle = document.querySelector( '.menu-toggle' );
+    const menu = document.querySelector( '.menu' );
+    new Pinecone.Menu(toggle, menu);
+
+    /* TODO: Resolve icon issues
+    const icons = document.querySelectorAll( 'svg' );
+
+    if ( icons.length > 0 ) {
+      Array.prototype.forEach.call( icons, icon => {
+        new Pinecone.Icon( icon );
+      } );
+    } */
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
