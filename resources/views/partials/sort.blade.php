091a3c5c3884b42b31529f9d73cc7764647a6708
Ned Zimmerman <ned@bight.ca>
2020-01-09 15:30:16 -0700

    Use proper partials

---
+++ resources/views/partials/sort.blade.php
@@ -0,0 +1,12 @@
+<div class="sort-wrapper">
+  <div class="sort">
+      <div class="menu-button">
+          <h2 class="h3 menu-button__label">@svg('sort', 'icon--sort', ['focusable' => 'false', 'aria-hidden' => 'true']) {{ __('Sort by', 'coop-library') }}</h2>
+          <ul class="link-list">
+              <li class="link-list__item"><a href="#" aria-current=true>{{ __('Date added', 'coop-library') }}</a></li>
+              <li class="link-list__item"><a href="#">{{ __('Date published', 'coop-library') }}</a></li>
+              <li class="link-list__item"><a href="#">{{ __('Most favorited', 'coop-library') }}</a></li>
+          </ul>
+      </div>
+  </div>
+</div>
