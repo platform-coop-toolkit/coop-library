b5ae9eb22d54c5ab974f9d35ca094f9e05700e83
Ned Zimmerman <ned@bight.dev>
2020-01-15 15:34:56 -0700

    fix: add function_exists check for language switcher

--- resources/views/partials/language-switcher.blade.php
+++ resources/views/partials/language-switcher.blade.php
@@ -1,3 +1,4 @@
+@if(function_exists('pll_the_languages'))
 <li class="menu-item menu-item--languages menu__submenu-wrapper">
   <a class="menu__item" href="{{ get_permalink(get_page_by_path('settings/language')) }}">
     <span class="menu__label">{{ __('Language', 'coop-library') }}</span>
@@ -11,3 +12,4 @@
     @endif
   </ul>
 </li>
+@endif
