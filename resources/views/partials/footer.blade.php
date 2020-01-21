a5fda341fe2649a8bccaad8e2f54c5b4fe5864c7
Ned Zimmerman <ned@bight.dev>
2020-01-21 15:04:12 -0700

    feat: add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,23 @@
 <footer class="content-info">
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+    <div class="columns">
+      <div class="column">
+        <a class="logo link--inverse" rel="external" href="https://platform.coop/">
+          {{-- TODO: Add logo svg --}}
+          <span class="screen-reader-text">{{ __('Platform Cooperativism Consortium', 'coop-library') }}</span>
+        </a>
+        <div class="nav">
+          <p><a class="link--inverse" rel="external" href="https://platform.coop/open-access-and-privacy-policy/">{{ __('Our open access and privacy policy', 'coop-library') }}</a></p>
+          <p><a class="link--inverse" rel="external" href="https://platform.coop/diversity-inclusion/">{{ __('Our commitment to diversity and inclusion', 'coop-library') }}</a></p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="h4"><a rel="external" class="link--inverse" href="https://platform.coop/contact-us/">Contact us</a></div>
+		    <div>
+          <strong>{{ __('Email', 'coop-library') }}</strong><br />
+			    <a class="link--inverse" href="mailto:{!! antispambot('info@platform.coop') !!}" rel="external">{!! antispambot('info@platform.coop') !!}</a>
+        </div>
+      </div>
+    </div>
   </div>
 </footer>
