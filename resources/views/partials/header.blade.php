87b36ce4c5d86f5dd3d0a5ea2ae40634c24f5b1f
Ned Zimmerman <ned@bight.ca>
2020-01-22 11:11:06 -0700

    feat: add inside wrapper to banner

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,24 +1,26 @@
 <header role="banner">
   <div class="container">
     <a class="brand"@if(is_front_page()) aria-current="page"@endif href="{{ home_url('/') }}">@svg('pcc', 'icon--pcc', ['focusable' => 'false', 'aria-hidden' => 'true'])<span class="brand__title screen-reader-text">{{ get_bloginfo('name', 'display') }}</span></a>
-    <nav aria-labelledby="menu-primary-label">
-      @if (has_nav_menu('primary_navigation'))
-        <button class="menu-toggle" aria-expanded="false">
-          @svg('menu', 'icon--open', ['focusable' => 'false', 'aria-hidden' => 'true'])
-          @svg('close', 'icon--close', ['focusable' => 'false', 'aria-hidden' => 'true'])
-          <span id="menu-primary-label" class="menu-toggle__label">Menu</span>
-        </button>
-        {!! wp_nav_menu([
-          'theme_location' => 'primary_navigation',
-          'menu_class' => (is_front_page()) ? 'menu menu--home' : 'menu',
-          'container' => false,
-          'before' => '<li class="%s">',
-          'after' => '</li>',
-          'before_submenu' => '<ul class="%s">',
-          'after_submenu' => '</ul>',
-          'walker' => new App\Walker()
-        ]) !!}
-      @endif
-    </nav>
+    <div class="inside">
+      <nav aria-labelledby="menu-primary-label">
+        @if (has_nav_menu('primary_navigation'))
+          <button class="menu-toggle" aria-expanded="false">
+            @svg('menu', 'icon--open', ['focusable' => 'false', 'aria-hidden' => 'true'])
+            @svg('close', 'icon--close', ['focusable' => 'false', 'aria-hidden' => 'true'])
+            <span id="menu-primary-label" class="menu-toggle__label">Menu</span>
+          </button>
+          {!! wp_nav_menu([
+            'theme_location' => 'primary_navigation',
+            'menu_class' => (is_front_page()) ? 'menu menu--home' : 'menu',
+            'container' => false,
+            'before' => '<li class="%s">',
+            'after' => '</li>',
+            'before_submenu' => '<ul class="%s">',
+            'after_submenu' => '</ul>',
+            'walker' => new App\Walker()
+          ]) !!}
+        @endif
+      </nav>
+    </div>
   </div>
 </header>
