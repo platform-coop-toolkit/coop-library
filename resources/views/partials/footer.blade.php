291072ccdd65f81899e4409f621774361a7ede2e
Ned Zimmerman <ned@bight.dev>
2020-01-23 15:56:25 -0700

    feat: add footer logo

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,7 +3,7 @@
     <div class="columns">
       <div class="column">
         <a class="logo link--inverse" rel="external" href="https://platform.coop/">
-          {{-- TODO: Add logo svg --}}
+          @svg('logo', 'logo__image', ['focusable' => 'false', 'aria-hidden' => 'true'])
           <span class="screen-reader-text">{{ __('Platform Cooperativism Consortium', 'coop-library') }}</span>
         </a>
         <div class="nav">
