f14eb34db97ff226c50679089eb15e20013aabd1
Ned Zimmerman <ned@bight.dev>
2020-01-22 20:56:19 -0700

    feat: use Koko Analytics for sorting by views

--- app/filters.php
+++ app/filters.php
@@ -106,8 +106,21 @@ add_filter('pre_get_posts', function ($query) {
                     $query->set('orderby', ['meta_value_num', 'date']);
                     break;
                 case 'viewed':
-                    $query->set('meta_key', 'lc_resource_views');
-                    $query->set('orderby', ['meta_value_num', 'date']);
+                    global $wpdb;
+                    $args = [
+                        'number'    => 5,
+                        'post_type' => 'lc_resource',
+                        'show_date' => false,
+                        'days'    => 30,
+                    ];
+                    $start_date = gmdate('Y-m-d', strtotime("-{$args['days']} days"));
+                    $end_date   = gmdate('Y-m-d', strtotime('tomorrow midnight'));
+                    $sql        = $wpdb->prepare("SELECT p.id, SUM(visitors) As visitors, SUM(pageviews) AS pageviews FROM {$wpdb->prefix}koko_analytics_post_stats s JOIN {$wpdb->posts} p ON s.id = p.id WHERE s.date >= %s AND s.date <= %s AND p.post_type = %s AND p.post_status = 'publish' GROUP BY s.id ORDER BY pageviews DESC LIMIT 0, %d", [$start_date, $end_date, $args['post_type'], $args['number']]);
+                    $results    = $wpdb->get_results($sql);
+                    $ids = ( ! empty($results) ) ? wp_list_pluck($results, 'id') : [];
+                    $query->set('post__in', $ids);
+                    $query->set('ignore_sticky_posts', true);
+                    $query->set('orderby', ['post__in', 'date']);
                     break;
                 case 'added':
                 default:
