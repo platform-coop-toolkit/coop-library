2d576bdc586891394bce003dd87d68fd9cbb5641
Ned Zimmerman <ned@bight.ca>
2020-01-24 10:27:11 -0700

    feat: use Koko Analytics for sorting by views (#155)
    * feat: use Koko Analytics for sorting by views
    * fix: coding standards error
    * fix: show all resources, even unviewed ones, when sorting by view
    * fix: apply new view order to home page
    * fix: home page order

--- app/filters.php
+++ app/filters.php
@@ -106,8 +106,28 @@ add_filter('pre_get_posts', function ($query) {
                     $query->set('orderby', ['meta_value_num', 'date']);
                     break;
                 case 'viewed':
-                    $query->set('meta_key', 'lc_resource_views');
-                    $query->set('orderby', ['meta_value_num', 'date']);
+                    global $wpdb;
+                    $args = [
+                        'post_type' => 'lc_resource',
+                        'show_date' => false,
+                        'days'    => 30,
+                    ];
+                    $start_date = gmdate('Y-m-d', strtotime("-{$args['days']} days"));
+                    $end_date   = gmdate('Y-m-d', strtotime('tomorrow midnight'));
+                    $sql        = $wpdb->prepare("SELECT p.id, SUM(visitors) As visitors, SUM(pageviews) AS pageviews FROM {$wpdb->prefix}koko_analytics_post_stats s JOIN {$wpdb->posts} p ON s.id = p.id WHERE s.date >= %s AND s.date <= %s AND p.post_type = %s AND p.post_status = 'publish' GROUP BY s.id ORDER BY pageviews DESC", [$start_date, $end_date, $args['post_type']]); // @codingStandardsIgnoreLine
+                    $results    = $wpdb->get_results($sql);
+                    $viewed_ids = ( ! empty($results) ) ? wp_list_pluck($results, 'id') : [];
+                    $unviewed_ids = get_posts([
+                        'orderby' => 'date',
+                        'order' => 'desc',
+                        'post_type' => 'lc_resource',
+                        'post__not_in' => $viewed_ids,
+                        'posts_per_page' => -1,
+                        'fields' => 'ids'
+                    ]);
+                    $ids = array_merge($viewed_ids + $unviewed_ids);
+                    $query->set('post__in', $ids);
+                    $query->set('orderby', 'post__in');
                     break;
                 case 'added':
                 default:
