69c501c8ad9f24d465de05b38cdc80a6922a8ebb
Ned Zimmerman <ned@bight.dev>
2020-02-11 21:49:23 -0700

    fix: use new pagination markup

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -21,6 +21,7 @@
     </ul>
   </div>
 
+  {{-- TODO: Replace this hack with a better implementation. --}}
   {!! str_replace(['page-numbers current', 'page-numbers'], ['page current', 'link link--pagination'], get_the_posts_pagination([
     'prev_text' => sprintf('&lsaquo; <span class="screen-reader-text">%s</span>', __('previous resources', 'coop-library')),
     'next_text' => sprintf(' <span class="screen-reader-text">%s</span> &rsaquo;', __('next resources', 'coop-library'))
