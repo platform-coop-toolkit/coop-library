0f08417eab5ae62a80418fb4a56287c3105aff46
Ned Zimmerman <ned@bight.dev>
2020-02-14 19:04:03 -0700

    feat: apply new resource layout

--- resources/views/partials/content-lc_resource.blade.php
+++ resources/views/partials/content-lc_resource.blade.php
@@ -28,7 +28,7 @@
       <span class="separator">.</span>
       @endif
       @if(Archive::getPublicationDate())
-      <span class="card__date"><span class="screen-reader-text">{{ __('date published', 'coop-library') }}: </span>2016</span>
+      <span class="card__date"><span class="screen-reader-text">{{ __('date published', 'coop-library') }}: </span>{{ Archive::getPublicationDate('Y') }}</span>
       @endif
     </div>
     @endif
