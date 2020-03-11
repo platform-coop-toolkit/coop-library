8242ed7e7d67ec862a9380a175c6504894888096
Ned Zimmerman <ned@bight.dev>
2020-03-11 15:47:55 -0600

    fix: add wrapper within page header to allow equal width stacked items

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,11 +1,13 @@
 <div class="page-header">
-  @if(App::breadcrumb())
-    <p class="breadcrumb">{!! App::breadcrumb() !!}</p>
-  @endif
-  <h1>{!! App::title() !!}</h1>
-  @if(is_front_page())
-  <p class="subhead">{{ get_bloginfo('description') }}</p>
-  @endif
+  <div class="inside">
+    @if(App::breadcrumb())
+      <p class="breadcrumb">{!! App::breadcrumb() !!}</p>
+    @endif
+    <h1>{!! App::title() !!}</h1>
+    @if(is_front_page())
+    <p class="subhead">{{ get_bloginfo('description') }}</p>
+    @endif
+  </div>
 </div>
 @if($notifications)
 @foreach($notifications as $notification)
