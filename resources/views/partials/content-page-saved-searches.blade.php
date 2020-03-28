88ebb6912f4802cc0d11dae113a6567411958a9a
Ned Zimmerman <ned@bight.dev>
2020-03-28 08:59:49 -0600

    fix: use different text for screen reader button labels (fix #208)

--- resources/views/partials/content-page-saved-searches.blade.php
+++ resources/views/partials/content-page-saved-searches.blade.php
@@ -1,6 +1,6 @@
 <div class="align-right" hidden>
   <button id="remove-all" class="button button--borderless button--destructive">
-    @svg('delete', 'icon--delete', ['focusable' => 'false', 'aria-hidden' => 'true']) {{ __('Remove all', 'coop-library') }}​​&nbsp;<span class="screen-reader-text">{{ __('saved searches', 'coop-library') }}</span>
+    @svg('delete', 'icon--delete', ['focusable' => 'false', 'aria-hidden' => 'true']) <span aria-hidden="true">{{ __('Remove all', 'coop-library') }}</span>​​<span class="screen-reader-text">{{ __('Remove all saved searches', 'coop-library') }}</span>
   </button>
 </div>
 <div id="saved-searches"></div>
--- resources/views/partials/current-filters.blade.php
+++ resources/views/partials/current-filters.blade.php
@@ -1,5 +1,29 @@
 @if(isset($_GET['s']) || $found_posts < App::totalPosts('lc_resource'))
 <div class="current-filters">
+  @php
+  global $wpdb;
+                        $args = [
+                            'post_type' => 'lc_resource',
+                            'show_date' => false,
+                            'days'    => 30,
+                        ];
+                        $start_date = gmdate('Y-m-d', strtotime("-{$args['days']} days"));
+                        $end_date   = gmdate('Y-m-d', strtotime('tomorrow midnight'));
+                        $sql        = $wpdb->prepare("SELECT p.id, SUM(visitors) As visitors, SUM(pageviews) AS pageviews FROM {$wpdb->prefix}koko_analytics_post_stats s JOIN {$wpdb->posts} p ON s.id = p.id WHERE s.date >= %s AND s.date <= %s AND p.post_type = %s AND p.post_status = 'publish' GROUP BY s.id ORDER BY pageviews DESC", [$start_date, $end_date, $args['post_type']]); // @codingStandardsIgnoreLine
+                        $results    = $wpdb->get_results($sql);
+                        $viewed_ids = ( ! empty($results) ) ? wp_list_pluck($results, 'id') : [];
+                        $unviewed_ids = get_posts([
+                            'orderby' => 'date',
+                            'order' => 'desc',
+                            'post_type' => 'lc_resource',
+                            'post__not_in' => $viewed_ids,
+                            'posts_per_page' => -1,
+                            'fields' => 'ids'
+                        ]);
+                        $ids = array_merge($viewed_ids + $unviewed_ids);
+                        echo count($viewed_ids);
+                        echo count($unviewed_ids);
+  @endphp
   <p class="h3">{{ sprintf(__('Showing %1$s of %2$s resources for:', 'coop-library'), $found_posts, App::totalPosts('lc_resource')) }}</p>
   @if(isset($_GET['s']))
   <h2 class="h4">{{ __('Search term', 'coop-library') }}</h2>
