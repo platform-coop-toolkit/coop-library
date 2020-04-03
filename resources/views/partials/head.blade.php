bafaa6daae141c56aed045e596fcfa092e82e9b2
Ned Zimmerman <ned@bight.dev>
2020-04-03 08:57:03 -0600

    feat: add favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,5 +3,7 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>
+  <link rel="shortcut icon" href="{{ App\asset_path('images/favicon.png') }}" type="image/png">
+  <link rel="shortcut icon" href="{{ App\asset_path('images/favicon.svg') }}" type="image/svg+xml">
   @php wp_head() @endphp
 </head>
