ba8b6ba8c0aef276b6a8ca7e7bd63429df1469a9
Ned Zimmerman <ned@bight.dev>
2020-09-04 09:23:22 -0300

    fix: ensure that searches are performed in the current language

--- resources/views/partials/search-form.blade.php
+++ resources/views/partials/search-form.blade.php
@@ -1,4 +1,4 @@
-<form role="search" method="get" class="search-form @if(isset($modifier))search-form--{{ $modifier }}@endif" action="{{ home_url() }}">
+<form role="search" method="get" class="search-form @if(isset($modifier))search-form--{{ $modifier }}@endif" action="{{ $home_url }}">
   <label>
     <span class="screen-reader-text">{{ __('Search for:', 'coop-library') }}</span>
     <input type="search" class="search-field"@if(isset($placeholder)) placeholder="{{ $placeholder }}"@endif value="@if(isset($_GET['s'])){{ $_GET['s'] }}@endif" name="s">
